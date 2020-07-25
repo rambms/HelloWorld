f4a1ef4d7ab992cae035702a995799c58d9d32e2
rambms <rambms07@gmail.com>
2020-07-25 23:01:04 +0530

    New class added

---
+++ HelloWorld/src/main/java/com/learn/Jenkins/GetWork.java
@@ -0,0 +1,20 @@
+package com.learn.Jenkins;
+
+public class GetWork {
+	String name;
+	String roll;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRoll() {
+		return roll;
+	}
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+	
+
+}
